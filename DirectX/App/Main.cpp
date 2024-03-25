d193f14b626d9965eaea1be4c58e771a43f031e2
KJB <kjb_jh@naver.com>
2024-03-25 17:29:52 +0900

    Changed the existing function pointer syntax to std::function.
    # 기존의 함수 포인터 문법을 std::function 으로 변경.

--- DirectX/App/Main.cpp
+++ DirectX/App/Main.cpp
@@ -6,6 +6,20 @@
 #pragma comment (lib, "EngineBase.lib")
 #pragma comment (lib, "EnginePlatform.lib")
 
+class EngineCore
+{
+public:
+	void Update()
+	{
+		int a = 0;
+	}
+
+	void End()
+	{
+		int a = 0;
+	}
+};
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR    lpCmdLine,
@@ -15,5 +29,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	UEngineWindow NewWindow;
 	NewWindow.Open();
 
-	UEngineWindow::WindowMessageLoop(nullptr, nullptr);
+	EngineCore Core;
+
+	UEngineWindow::WindowMessageLoop(
+		std::bind(&EngineCore::Update, &Core),
+		std::bind(&EngineCore::End, &Core)
+	);
 }
