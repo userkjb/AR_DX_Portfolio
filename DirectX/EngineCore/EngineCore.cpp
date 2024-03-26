6924b9e5cda2633fee50004fa7506d6a755700c3
KJB <kjb_jh@naver.com>
2024-03-26 16:52:58 +0900

    Keyboard input check completed.
    # 키보드 Input 체크 완료.

--- DirectX/EngineCore/EngineCore.cpp
+++ DirectX/EngineCore/EngineCore.cpp
@@ -1,11 +1,9 @@
 #include "PreCompile.h"
 #include "EngineCore.h"
-#include "EngineOption.h"
+
 #include <EngineBase/EngineFile.h>
 #include <EngineBase/EngineDirectory.h>
-
 #include <EnginePlatform/EngineSound.h>
-#include <EnginePlatform/EngineInput.h>
 
 UEngineCore::UEngineCore()
 {
@@ -13,21 +11,28 @@ UEngineCore::UEngineCore()
 
 UEngineCore::~UEngineCore()
 {
+	// 엔진이 종료할때 기존 엔진 옵션을 세이브 하고 한다.
+	UEngineDirectory Dir;
+	Dir.MoveToSearchChild("Config");
+	UEngineFile File = Dir.GetPathFromFile("EngineOption.EData");
+	UEngineSerializer Ser;
+	EngineOption.Serialize(Ser);
+	File.Open(EIOOpenMode::Write, EIODataType::Text);
+	File.Save(Ser);
 }
 
 UEngineCore* GEngine = nullptr;
 
-void UEngineCore::EngineStart(HINSTANCE _Inst)
+void UEngineCore::EngineOptionInit()
 {
 	UEngineDirectory Dir;
 	Dir.MoveToSearchChild("Config");
 
-	FEngineOption Option;
 	if (false == Dir.IsFile("EngineOption.EData"))
 	{
 		UEngineFile File = Dir.GetPathFromFile("EngineOption.EData");
 		UEngineSerializer Ser;
-		Option.Serialize(Ser);
+		EngineOption.Serialize(Ser);
 
 		File.Open(EIOOpenMode::Write, EIODataType::Text);
 		File.Save(Ser);
@@ -39,33 +44,30 @@ void UEngineCore::EngineStart(HINSTANCE _Inst)
 		File = Dir.GetPathFromFile("EngineOption.EData");
 		File.Open(EIOOpenMode::Read, EIODataType::Text);
 		File.Load(Ser);
-		Option.DeSerialize(Ser);
+		EngineOption.DeSerialize(Ser);
 	}
+}
 
-	EngineWindow.Open(Option.WindowTitle);
-	EngineWindow.SetWindowScale(Option.WindowScale);
+void UEngineCore::EngineStart(HINSTANCE _Inst)
+{
+	EngineOptionInit();
 
-	UserCorePtr->Initialize();
+	EngineWindow.Open(EngineOption.WindowTitle);
+	EngineWindow.SetWindowScale(EngineOption.WindowScale);
 
-	// 사운드 체크.
 	{
-		UEngineDirectory Dir;
-		Dir.MoveToSearchChild("ContentsResources");
-		Dir.MoveToSearchChild("Sound");
-		std::list<UEngineFile> Files = Dir.AllFile({ ".mp3" });
-
-		for (UEngineFile File : Files)
-		{
-			UEngineSound::Load(File.GetFullPath());
-		}
-
-		UEngineSound::SoundPlay("Ready.mp3");
+		UserCorePtr->Initialize();
+		MainTimer.TimeCheckStart();
 	}
 
 	UEngineWindow::WindowMessageLoop(
-		nullptr,
+		std::bind(&UEngineCore::EngineUpdate, this),
 		nullptr
-		//std::bind(&UEngineCore::Update, &Core),
-		//std::bind(&UEngineCore::End, &Core)
 	);
+}
+
+void UEngineCore::EngineUpdate()
+{
+	float DeltaTime = MainTimer.TimeCheck();
+	UEngineInput::KeyCheckTick(DeltaTime);
 }
