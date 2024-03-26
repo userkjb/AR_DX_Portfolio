f0d21076e31de9785699e6bfeafe3ce3db390b73
KJB <kjb_jh@naver.com>
2024-03-26 15:25:33 +0900

    App Main function modified.
    # App Main함수 수정.

--- DirectX/App/Main.cpp
+++ DirectX/App/Main.cpp
@@ -1,38 +1,18 @@
 #include <Windows.h>
 #include <string_view>
 
-#include <EnginePlatform/EngineWindow.h>
+#include <EngineCore/EngineCore.h>
+#include <Contents/ContentsCore.h>
 
 #pragma comment (lib, "EngineBase.lib")
 #pragma comment (lib, "EnginePlatform.lib")
-
-class EngineCore
-{
-public:
-	void Update()
-	{
-		int a = 0;
-	}
-
-	void End()
-	{
-		int a = 0;
-	}
-};
+#pragma comment (lib, "EngineCore.lib")
+#pragma comment (lib, "Contents.lib")
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR    lpCmdLine,
 	_In_ int       nCmdShow)
 {
-	//	LeakCheck; 
-	UEngineWindow NewWindow;
-	NewWindow.Open();
-
-	EngineCore Core;
-
-	UEngineWindow::WindowMessageLoop(
-		std::bind(&EngineCore::Update, &Core),
-		std::bind(&EngineCore::End, &Core)
-	);
+	UEngineCore::Start<UContentsCore>(hInstance);
 }
