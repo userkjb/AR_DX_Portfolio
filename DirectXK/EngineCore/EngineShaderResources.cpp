857968f2d46b4822b7a8fde9259359ed7db70092
KJB <kjb_jh@naver.com>
2024-04-03 21:09:25 +0900

    Declaration of a function to check ShaderResources.
    # ShaderResources를 Check하는 함수 선언.

--- DirectXK/EngineCore/EngineShaderResources.cpp
+++ DirectXK/EngineCore/EngineShaderResources.cpp
@@ -1,2 +1,7 @@
 #include "PreCompile.h"
 #include "EngineShaderResources.h"
+
+void UEngineShaderResources::ShaderResourcesCheck(std::string_view _EntryName, ID3DBlob* _ShaderCode)
+{
+
+}
--- DirectXK/EngineCore/EngineShaderResources.h
+++ DirectXK/EngineCore/EngineShaderResources.h
@@ -1,5 +1,12 @@
 #pragma once
+
+// Ό³Έν :
 class UEngineShaderResources
 {
-};
+public:
 
+protected:
+
+private:
+	void ShaderResourcesCheck(std::string_view _EntryName, ID3DBlob* _ShaderCode);
+};
