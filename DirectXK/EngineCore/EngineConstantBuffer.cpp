63b3d42f37a5428e8a9e0b523e872dc99f2d9f0a
KJB <kjb_jh@naver.com>
2024-04-03 22:08:33 +0900

    Since ConstantBuffers are static, implemented in CPP.
    # ConstantBuffers가 static 이므로 cpp에서 구현.

--- DirectXK/EngineCore/EngineConstantBuffer.cpp
+++ DirectXK/EngineCore/EngineConstantBuffer.cpp
@@ -1,6 +1,8 @@
 #include "PreCompile.h"
 #include "EngineConstantBuffer.h"
 
+std::map<EShaderType, std::map<std::string, std::map<int, std::shared_ptr<UEngineConstantBuffer>>>> UEngineConstantBuffer::ConstantBuffers;
+
 UEngineConstantBuffer::UEngineConstantBuffer()
 {
 }
