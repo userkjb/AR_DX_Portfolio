89978bd8c2bfa139c9afb287b2214ffb71ae81ae
KJB <kjb_jh@naver.com>
2024-04-04 03:55:35 +0900

    If you input a key in the Player and allow it to move, you can see the red square moving.
    # Player에서 키를 입력 받아 움직일 수 있도록 해주면 붉은 네모가 움직이는 것을 확인할 수 있다.

--- DirectXK/Contents/Player.cpp
+++ DirectXK/Contents/Player.cpp
@@ -5,9 +5,15 @@
 APlayer::APlayer()
 {
 	Renderer = CreateDefaultSubObject<URenderer>("Renderer");
-
 	Renderer->SetMesh("Rect");
+	// 메테리얼을 해주는순간
+	// 메테리얼은 안에 버텍스쉐이더와 픽셀쉐이더를 둘다 들고 있죠.
+	// 색깔  머티리얼그자체에 세팅해준다.
+	// 
 	Renderer->SetMaterial("2DImage");
+
+	// Renderer->세팅픽셀쉐이더상수버퍼();
+	// Renderer->세팅버텍스쉐이더상수버퍼();
 }
 
 APlayer::~APlayer()
@@ -18,14 +24,38 @@ void APlayer::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FTransform& Transform = GetActorTransform();
-	Renderer->Resources->SettingConstantBuffer("FTransform", Transform);
+	SetActorScale3D(FVector(100.0f, 100.0f, 100.0f));
 }
 
 void APlayer::Tick(float _DeltaTime)
 {
 	// 위에 뭔가를 쳐야할때도 있다.
 	Super::Tick(_DeltaTime);
+
+	float Speed = 100.0f;
+
+	if (true == UEngineInput::IsPress('A'))
+	{
+		AddActorLocation(FVector::Left * _DeltaTime * Speed);
+	}
+
+	if (true == UEngineInput::IsPress('D'))
+	{
+		AddActorLocation(FVector::Right * _DeltaTime * Speed);
+	}
+
+	if (true == UEngineInput::IsPress('W'))
+	{
+		AddActorLocation(FVector::Up * _DeltaTime * Speed);
+	}
+
+	if (true == UEngineInput::IsPress('S'))
+	{
+		AddActorLocation(FVector::Down * _DeltaTime * Speed);
+	}
+
+
+	// Renderer->SetConstanctBuffer("Ftransform", GetTransform());
 
 
 }
