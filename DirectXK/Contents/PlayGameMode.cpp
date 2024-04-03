cfdf3277ce5887befba595942191f2ad0cd3b225
KJB <kjb_jh@naver.com>
2024-04-04 03:54:41 +0900

    Set the initial value of the camera in GameMode.
    # GameMode 에서 카메라의 초기 값을 설정해 준다.

--- DirectXK/Contents/PlayGameMode.cpp
+++ DirectXK/Contents/PlayGameMode.cpp
@@ -1,6 +1,7 @@
 #include "PreCompile.h"
 #include "PlayGameMode.h"
 #include "Player.h"
+#include <EngineCore/Camera.h>
 
 APlayGameMode::APlayGameMode()
 {
@@ -13,11 +14,16 @@ APlayGameMode::~APlayGameMode()
 void APlayGameMode::BeginPlay()
 {
 	Super::BeginPlay();
+
+	std::shared_ptr<UCamera> Camera = GetWorld()->GetMainCamera();
+	Camera->SetActorLocation(FVector(0.0f, 0.0f, -100.0f));
+
 	GetWorld()->SpawnActor<APlayer>("Player");
 }
 
 void APlayGameMode::Tick(float _DeltaTime)
 {
 	Super::Tick(_DeltaTime);
+
 
 }
