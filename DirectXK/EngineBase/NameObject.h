153dbd5aad0368a6da59fde497e97056f77ccd31
KJB <kjb_jh@naver.com>
2024-04-07 01:10:29 +0900

    Removed Delete functions from NameObject.
    # NameObject 에 Delete 함수들 제거.

--- DirectXK/EngineBase/NameObject.h
+++ DirectXK/EngineBase/NameObject.h
@@ -10,12 +10,6 @@ public:
 	UNameObject();
 	virtual ~UNameObject();
 
-	// delete Function
-	UNameObject(const UNameObject& _Other) = delete;
-	UNameObject(UNameObject&& _Other) noexcept = delete;
-	UNameObject& operator=(const UNameObject& _Other) = delete;
-	UNameObject& operator=(UNameObject&& _Other) noexcept = delete;
-
 	void SetName(std::string_view _View)
 	{
 		Name = UEngineString::ToUpper(_View);
