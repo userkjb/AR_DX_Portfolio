7907ffc5e8e1e79c7155db8e9d02abf63c1d7f19
KJB <kjb_jh@naver.com>
2024-04-08 14:09:09 +0900

    Modified so that resources can be read at each level.
    # 각 레벨에서 리소스들을 읽을 수 있도록 수정.

--- DirectXK/Contents/TitleGameMode.cpp
+++ DirectXK/Contents/TitleGameMode.cpp
@@ -16,10 +16,26 @@ void ATitleGameMode::BeginPlay()
 {
 	Super::BeginPlay();
 
+	UEngineDirectory Dir;
+	Dir.MoveToSearchChild("ContentsResources");
+	Dir.Move("Image\\GameTitle");
+	std::vector<UEngineFile> Files = Dir.GetAllFile({ ".png" }, true);
+	for (UEngineFile& File : Files)
+	{
+		// CuttingTest.png texture로도 한장이 로드가 됐고
+		// 스프라이트로도 1장짜리로 로드가 된 상황이야.
+		UEngineTexture::Load(File.GetFullPath());
+	}
+
+	std::vector<UEngineDirectory> Directorys = Dir.GetAllDirectory();
+	for (size_t i = 0; i < Directorys.size(); i++)
+	{
+		std::string Name = Directorys[i].GetFolderName();
+		UEngineSprite::LoadFolder(Directorys[i].GetFullPath());
+	}
+
 	std::shared_ptr<UCamera> Camera = GetWorld()->GetMainCamera();
 	Camera->SetActorLocation(FVector(0.0f, 0.0f, -100.0f));
-
-	GetWorld()->SpawnActor<AGameLogo>("GameLogo");
 }
 
 void ATitleGameMode::Tick(float _DeltaTime)
@@ -35,4 +51,6 @@ void ATitleGameMode::LevelEnd(ULevel* _NextLevel)
 void ATitleGameMode::LevelStart(ULevel* _PrevLevel)
 {
 	Super::LevelStart(_PrevLevel);
+
+	GetWorld()->SpawnActor<AGameLogo>("GameLogo");
 }
