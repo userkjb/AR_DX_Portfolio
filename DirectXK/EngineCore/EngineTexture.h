a6f46fd412e12468e1060b353f5a31ec3f354d56
KJB <kjb_jh@naver.com>
2024-04-09 16:16:25 +0900

    Create a function to get the Color for pixel collision.
    # 픽셀 충돌을 위해 Color를 가져오는 함수 생성.

--- DirectXK/EngineCore/EngineTexture.h
+++ DirectXK/EngineCore/EngineTexture.h
@@ -14,70 +14,6 @@
 
 #include "EngineSampler.h"
 
-//class UEngineResourcesUEngineTexture
-//{
-//public:
-//	// constrcuter destructer
-//	UEngineResources() {}
-//	~UEngineResources() {}
-//
-//	// delete Function
-//	UEngineResources(const UEngineResources& _Other) = delete;
-//	UEngineResources(UEngineResources&& _Other) noexcept = delete;
-//	UEngineResources& operator=(const UEngineResources& _Other) = delete;
-//	UEngineResources& operator=(UEngineResources&& _Other) noexcept = delete;
-//
-//	static std::shared_ptr<UEngineTexture> CreateResName(std::string_view& _Name, std::string_view _Path)
-//	{
-//		std::string UpperName = UEngineString::ToUpper(_Name);
-//
-//		std::shared_ptr<UEngineTexture> NewRes = std::make_shared<UEngineTexture>();
-//		NewRes->Name = _Name;
-//		NewRes->Path = _Path;
-//
-//		if (true == NameResources.contains(UpperName))
-//		{
-//			MsgBoxAssert("이미 존재하는 리소스를 또 로드하려고 했습니다." + UpperName);
-//		}
-//
-//		NameResources[UpperName] = NewRes;
-//		return NewRes;
-//	}
-//
-//	static std::shared_ptr<UEngineTexture> CreateResUnName()
-//	{
-//		std::shared_ptr<ResType> NewRes = std::make_shared<UEngineTexture>();
-//		UnNameResources.push_back(NewRes);
-//		return NewRes;
-//	}
-//
-//
-//protected:
-//
-//private:
-//	// 당연히 그 자료형 그대로 쓰고
-//	// 텍스처면 텍스처 그대로
-//	// 랜더타겟이면 랜더타겟 그대로
-//	// 사운드면 사운드 그대로 쓰고 싶을 것이다.
-//	static std::map<std::string, std::shared_ptr<UEngineTexture>> NameResources;
-//	static std::list<std::shared_ptr<UEngineTexture>> UnNameResources;
-//};
-//
-//// 템플릿일때는 문제가 없습니다.
-//// 전역변수는 헤더에 추가할수가 없죠?
-//// 템플릿이면 무넺가 해결됩니다.
-//// 템플릿 static 변수를 선언하는 방법을 배워야 한다.
-//template<typename ResType>
-//std::map<std::string, std::shared_ptr<ResType>> UEngineResources<ResType>::NameResources;
-//
-//// 템플릿 static 변수 선언하는법.
-//template<typename ResType>
-//std::list<std::shared_ptr<ResType>> UEngineResources<ResType>::UnNameResources;
-
-// 설명 :
-// 템플릿
-// class UEngineTexture : public UEngineResourcesUEngineTexture
-// 사용하기나 관리하기가 너무 힘드니까
 class UEngineTextureSetter;
 class UEngineTexture : public UEngineResources<UEngineTexture>
 {
@@ -114,6 +50,7 @@ public:
 		return NewRes;
 	}
 
+
 	ID3D11RenderTargetView* GetRTV()
 	{
 		return RTV;
@@ -123,6 +60,20 @@ public:
 	{
 		return float4(Desc.Width, Desc.Height);
 	}
+
+	// float 자체가 오차의 자료형
+	// 대략적인 값 실수오차는 무조건 발생한다.
+
+	// float4 GetColor(unsigned)
+
+	// Color8Bit _DefaultColor => 화면 바깥에 있는 컬러를 가져와 달라고 하면  _DefaultColor가 리턴된다.
+
+	Color8Bit GetColor(float4 _Pos, Color8Bit _DefaultColor)
+	{
+		return GetColor(_Pos.iX(), _Pos.iY(), _DefaultColor);
+	}
+
+	Color8Bit GetColor(unsigned int _X, unsigned int _Y, Color8Bit _DefaultColor);
 
 protected:
 
@@ -145,7 +96,7 @@ private:
 	DirectX::ScratchImage Image;
 
 	std::shared_ptr<UEngineSampler> Sampler;
-	
+
 	void ResCreate(ID3D11Texture2D* _Texture);
 
 	void ResLoad();
