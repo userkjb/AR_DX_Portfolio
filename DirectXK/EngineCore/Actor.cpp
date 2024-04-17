08a934612cd47adc71525b3d91ef46f08c8c84c5
KJB <kjb_jh@naver.com>
2024-04-17 14:49:19 +0900

    [Engine] Add WidgetInit function to Actor.
    # Actor에 WidgetInit 함수 추가.

--- DirectXK/EngineCore/Actor.cpp
+++ DirectXK/EngineCore/Actor.cpp
@@ -2,6 +2,7 @@
 #include "Actor.h"
 #include "SceneComponent.h"
 #include "DefaultSceneComponent.h"
+#include "Widget.h"
 
 std::set<AActor*> AActor::InputActors;
 std::set<AActor*> AActor::PrevInputActors;
@@ -168,7 +169,7 @@ void AActor::SetActorScale3D(FVector _Value)
 #ifdef _DEBUG
 	if (nullptr == RootComponent)
 	{
-		MsgBoxAssert("루트 컴포넌트가 지정되지 않았습니다.");
+		MsgBoxAssert("루트 컴포넌트가 지정되지 않았습니다." + GetName());
 	}
 #endif
 
@@ -347,4 +348,12 @@ bool AActor::IsAnykeyFree()
 
 	return UEngineInput::IsAnykeyFree();
 
+}
+
+void AActor::WidgetInit(std::shared_ptr<UWidget> _Widget, std::string_view _Name)
+{
+	_Widget->SetWorld(GetWorld());
+	_Widget->SetName(_Name);
+
+	// GetWorld()->PushWidget(_Widget);
 }
