272f3b3fd385a343917bcce794d555d506e53082
KJB <kjb_jh@naver.com>
2024-04-18 01:06:49 +0900

    Added AutoSize constant to helper.
    # 헬퍼에 AutoSize 상수 추가.

--- DirectXK/Contents/ContentsConstValue.h
+++ DirectXK/Contents/ContentsConstValue.h
@@ -4,6 +4,7 @@ class UContentsConstValue
 {
 public :
 	static const float TileSize;
+	static const float AutoSizeValue;
 	static std::shared_ptr<UEngineTexture> MapTex;
 	static float4 MapTexScale;
 };
