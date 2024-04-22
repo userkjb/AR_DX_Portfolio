d73f5d1b7241e1d4a8c9ae1735e71d12972cb8f7
KJB <kjb_jh@naver.com>
2024-04-22 10:33:53 +0900

    Fixed mouse angle issue.
    # 마우스 각도 문제 해결.

--- DirectXK/Contents/Player.cpp
+++ DirectXK/Contents/Player.cpp
@@ -56,31 +56,35 @@ void APlayer::Tick(float _DeltaTime)
 	PixelCheck(_DeltaTime);
 
 	{
-		// 여기가 문제!!!!
 		PlayerPos = GetActorLocation();
 		float4 CulMousPos = GEngine->EngineWindow.GetScreenMousePos();
+		float4 MousePosWorld = GetWorld()->GetMainCamera()->ScreenPosToWorldPos(CulMousPos);
+
 		FVector ScreenScale = GEngine->EngineWindow.GetWindowScale();
-		CulMousPos.Y -= ScreenScale.Y;
-		CulMousPos.Y *= -1.0f;
+		//CulMousPos.Y -= ScreenScale.Y;
+		//CulMousPos.Y *= -1.0f;
 		
-		float4 Leng = CulMousPos - PlayerPos;
+		//float4 Leng = CulMousPos - PlayerPos;
+		float4 Leng = MousePosWorld - PlayerPos;
 		PlayerToMouseDir = Leng.Normalize3DReturn();
 
 		////
 		// 라디안 각도.
-		float Rot = atan2((CulMousPos.Y - PlayerPos.Y), (CulMousPos.X - PlayerPos.X));
+		//float Rot = atan2((CulMousPos.Y - PlayerPos.Y), (CulMousPos.X - PlayerPos.X));
+		float Rot = atan2((MousePosWorld.Y - PlayerPos.Y), (MousePosWorld.X - PlayerPos.X));
 		Rot *= UEngineMath::RToD; // 디그리( 0 ~ 180)
 		WeaponDir.Z = Rot;
 		////
 #ifdef _DEBUG
 		//std::string Msg1 = std::format("Screen : {}\n", ScreenScale.ToString());
-		std::string Msg2 = std::format("Player Pos : {}\n", PlayerPos.ToString());
-		std::string Msg3 = std::format("Mouses Pos : {}\n", CulMousPos.ToString());
-		//std::string Msg4 = std::format("Leng : {}\n", Leng.ToString());
-		//std::string Msg5 = std::format("PlayerToMouseDir : {}\n", PlayerToMouseDir.ToString());
-		//std::string Msg6 = std::format("Leng : {}\n", Leng.ToString());
-		std::string Msg7 = std::format("Rot : {}\n", Rot);
-		//std::string Msg8 = std::format("CalVectors : {}\n", CalVectors.ToString());
+		std::string Msg2 = std::format("Player_World Pos : {}\n", PlayerPos.ToString());
+		std::string Msg3 = std::format("Mouses_World Pos : {}\n", MousePosWorld.ToString());
+		//std::string Msg4 = std::format("Mouses_World Pos : {}\n", MousePosWorld.ToString());
+		//std::string Msg5 = std::format("Leng : {}\n", Leng.ToString());
+		//std::string Msg6 = std::format("PlayerToMouseDir : {}\n", PlayerToMouseDir.ToString());
+		//std::string Msg7 = std::format("Leng : {}\n", Leng.ToString());
+		std::string Msg8 = std::format("Rot : {}\n", Rot);
+		//std::string Msg9 = std::format("CalVectors : {}\n", CalVectors.ToString());
 
 		//UEngineDebugMsgWindow::PushMsg(Msg1);
 		UEngineDebugMsgWindow::PushMsg(Msg2);
@@ -88,8 +92,9 @@ void APlayer::Tick(float _DeltaTime)
 		//UEngineDebugMsgWindow::PushMsg(Msg4);
 		//UEngineDebugMsgWindow::PushMsg(Msg5);
 		//UEngineDebugMsgWindow::PushMsg(Msg6);
-		UEngineDebugMsgWindow::PushMsg(Msg7);
-		//UEngineDebugMsgWindow::PushMsg(Msg8);
+		//UEngineDebugMsgWindow::PushMsg(Msg7);
+		UEngineDebugMsgWindow::PushMsg(Msg8);
+		//UEngineDebugMsgWindow::PushMsg(Msg9);
 #endif
 	}
 
