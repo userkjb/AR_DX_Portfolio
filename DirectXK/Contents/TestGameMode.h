63a9bfeac081e4566dc2dc26582c704af4fdc9cb
KJB <kjb_jh@naver.com>
2024-04-27 11:01:26 +0900

    Thread Application Test.
    # Thread 적용 테스트.

--- DirectXK/Contents/TestGameMode.h
+++ DirectXK/Contents/TestGameMode.h
@@ -17,6 +17,8 @@ public:
 	ATestGameMode& operator=(const ATestGameMode& _Other) = delete;
 	ATestGameMode& operator=(ATestGameMode&& _Other) noexcept = delete;
 
+	std::atomic_int LoadingCount; // Thread
+
 protected:
 	void BeginPlay() override;
 	void Tick(float _DeltaTime) override;
@@ -25,6 +27,7 @@ protected:
 	void LevelEnd(ULevel* _NextLevel) override;
 
 private:
+	std::vector<std::shared_ptr<UEngineThread>> LoadingThreads; // Thread
 	std::shared_ptr<UCamera> Camera = nullptr;
 };
 
