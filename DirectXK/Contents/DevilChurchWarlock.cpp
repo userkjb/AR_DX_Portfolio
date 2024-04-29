9fc3fee2ced2db90d0fef648fc061cee73e5e540
KJB <kjb_jh@naver.com>
2024-04-29 21:36:45 +0900

    Written by Shinja Class Base.
    # 신자 Class Base 작성.

--- DirectXK/Contents/DevilChurchWarlock.cpp
+++ DirectXK/Contents/DevilChurchWarlock.cpp
@@ -1,2 +1,26 @@
 #include "PreCompile.h"
 #include "DevilChurchWarlock.h"
+#include <EngineCore/DefaultSceneComponent.h>
+
+ADevilChurchWarlock::ADevilChurchWarlock()
+{
+	Root = CreateDefaultSubObject<UDefaultSceneComponent>("Render");
+	SetRoot(Root);
+}
+
+ADevilChurchWarlock::~ADevilChurchWarlock()
+{
+}
+
+
+void ADevilChurchWarlock::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ADevilChurchWarlock::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+
+}
--- DirectXK/Contents/DevilChurchWarlock.h
+++ DirectXK/Contents/DevilChurchWarlock.h
@@ -1,5 +1,28 @@
 #pragma once
-class ADevilChurchWarlock
+#include <EngineCore/Actor.h>
+
+class USpriteRenderer;
+class UDefaultSceneComponent;
+class ADevilChurchWarlock : public AActor
 {
+	GENERATED_BODY(AActor)
+
+public:
+	// constrcuter destructer
+	ADevilChurchWarlock();
+	~ADevilChurchWarlock();
+
+	// delete Function
+	ADevilChurchWarlock(const ADevilChurchWarlock& _Other) = delete;
+	ADevilChurchWarlock(ADevilChurchWarlock&& _Other) noexcept = delete;
+	ADevilChurchWarlock& operator=(const ADevilChurchWarlock& _Other) = delete;
+	ADevilChurchWarlock& operator=(ADevilChurchWarlock&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private :
+	UDefaultSceneComponent* Root = nullptr;
 };
 
