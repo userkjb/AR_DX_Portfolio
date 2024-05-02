35d6a18b2eb31c6cb23f30a5645e561098263e6d
KJB <kjb_jh@naver.com>
2024-05-02 13:39:09 +0900

    Player collision size and position settings.
    # Player 콜리전 크기, 위치 설정.

--- DirectXK/Contents/Player.cpp
+++ DirectXK/Contents/Player.cpp
@@ -15,7 +15,8 @@ APlayer::APlayer()
 
 	PlayerCollision = CreateDefaultSubObject<UCollision>("Collision");
 	PlayerCollision->SetupAttachment(Root);
-	PlayerCollision->SetScale(FVector(16.0f, 16.0f));
+	PlayerCollision->SetScale(FVector(64.0f, 64.0f));
+	PlayerCollision->AddPosition(FVector(0.0f, 32.0f));
 	// 콜리전은 무조건 오더를 지정해줘야 한다.
 	PlayerCollision->SetCollisionGroup(ECollisionOrder::Player);
 	PlayerCollision->SetCollisionType(ECollisionType::RotRect);
