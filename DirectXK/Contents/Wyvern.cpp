315b94412fc8d6a99346fe7c3686aeb090294498
KJB <kjb_jh@naver.com>
2024-05-02 17:35:10 +0900

    Written by Wyvern Base.
    # Wyvern Base 작성.

--- DirectXK/Contents/Wyvern.cpp
+++ DirectXK/Contents/Wyvern.cpp
@@ -1,2 +1,20 @@
 #include "PreCompile.h"
 #include "Wyvern.h"
+
+AWyvern::AWyvern()
+{
+}
+
+AWyvern::~AWyvern()
+{
+}
+
+void AWyvern::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AWyvern::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+}
--- DirectXK/Contents/Wyvern.h
+++ DirectXK/Contents/Wyvern.h
@@ -1,5 +1,24 @@
 #pragma once
-class AWyvern
+#include <EngineCore/Actor.h>
+
+class AWyvern : public AActor
 {
+	GENERATED_BODY(AActor)
+public:
+	// constrcuter destructer
+	AWyvern();
+	~AWyvern();
+
+	// delete Function
+	AWyvern(const AWyvern& _Other) = delete;
+	AWyvern(AWyvern&& _Other) noexcept = delete;
+	AWyvern& operator=(const AWyvern& _Other) = delete;
+	AWyvern& operator=(AWyvern&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private:
 };
 
