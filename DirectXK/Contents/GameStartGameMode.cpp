546f67c6f1de06e8a269c6dca098ea69b7d8d344
KJB <kjb_jh@naver.com>
2024-05-03 13:56:31 +0900

    Skeleton image recall and cutting.
    # 스켈레톤 이미지 호출 및 커팅.

--- DirectXK/Contents/GameStartGameMode.cpp
+++ DirectXK/Contents/GameStartGameMode.cpp
@@ -17,22 +17,37 @@ void AGameStartGameMode::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UEngineDirectory Dir;
-	Dir.MoveToSearchChild("ContentsResources");
-	Dir.Move("Image\\PlayerActor");
-	std::vector<UEngineFile> Files = Dir.GetAllFile({ ".png" }, true);
-	for (UEngineFile& File : Files)
 	{
-		// CuttingTest.png texture로도 한장이 로드가 됐고
-		// 스프라이트로도 1장짜리로 로드가 된 상황이야.
-		UEngineSprite::Load(File.GetFullPath());
-	}
+		UEngineDirectory Dir;
+		Dir.MoveToSearchChild("ContentsResources");
+		Dir.Move("Image\\PlayerActor");
+		std::vector<UEngineFile> Files = Dir.GetAllFile({ ".png" }, true);
+		for (UEngineFile& File : Files)
+		{
+			// CuttingTest.png texture로도 한장이 로드가 됐고
+			// 스프라이트로도 1장짜리로 로드가 된 상황이야.
+			UEngineSprite::Load(File.GetFullPath());
+		}
 
-	std::vector<UEngineDirectory> Directorys = Dir.GetAllDirectory();
-	for (size_t i = 0; i < Directorys.size(); i++)
+		std::vector<UEngineDirectory> Directorys = Dir.GetAllDirectory();
+		for (size_t i = 0; i < Directorys.size(); i++)
+		{
+			std::string Name = Directorys[i].GetFolderName();
+			UEngineSprite::LoadFolder(Directorys[i].GetFullPath());
+		}
+	}
 	{
-		std::string Name = Directorys[i].GetFolderName();
-		UEngineSprite::LoadFolder(Directorys[i].GetFullPath());
+		UEngineDirectory Dir;
+		Dir.MoveToSearchChild("ContentsResources");
+		Dir.Move("Image\\GlobalMonster\\Skeleton");
+		std::vector<UEngineFile> Files = Dir.GetAllFile({ ".png" }, true);
+		for (UEngineFile& File : Files)
+		{
+			UEngineSprite::Load(File.GetFullPath());
+		}
+		UEngineSprite::CreateCutting("sk_Attack.png", 6, 2);
+		UEngineSprite::CreateCutting("sk_Idle.png", 5, 1);
+		UEngineSprite::CreateCutting("sk_Run.png", 4, 2);
 	}
 
 	std::shared_ptr<UCamera> Camera = GetWorld()->GetMainCamera();
